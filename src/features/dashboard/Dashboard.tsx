75c16c55cb5f64e759819060420363f6e36d55ff
Richard Ayikwei <richardayikweibee@gmail.com>
2024-12-24 20:17:11 +0000

    fixed calcs

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -13,6 +13,8 @@ const Dashboard = () => {
 
   let additionalUsers =
     (dashboardState.batteriesSwapStations * 2 - usersBatteries) / 2;
+
+  let realisticUsers = Math.floor(73 / 2);
   return (
     <section className="flex mt-16">
       <div className="w-80">
@@ -22,31 +24,37 @@ const Dashboard = () => {
             {Number.isNaN(reserveRatio) ? 0 : reserveRatio.toFixed(4)}
           </p>
         </div>
-        <div className="w-40 shadow-2xl text-center p-2 mt-10">
-          <div title="Additional users that can be added to network">
+        <div className="w-72 shadow-2xl text-center p-2 mt-10 flex flex-row">
+          <div title="Additional users that can be added to network based on current batteries" className="w-36">
             <h2>Additional users:</h2>
             <p>{additionalUsers}</p>
+          </div>
+          <div title="Realistic users that can be added to network based on new batteries available" className="border-l-2 w-36" >
+            <h2>Realitic users:</h2>
+            <p>{realisticUsers}</p>
           </div>
         </div>
       </div>
       <div className="flex w-80">
         <div>
           <input
+            placeholder={dashboardState.activeUsers}
             onChange={(e) => dispatch(addUsers(Number(e.target.value)))}
             title="Active users"
             id="users"
             type="number"
-            className="bg-orange-400 flex flex-col h-36 text-7xl w-40 text-center"
+            className="bg-orange-400 flex flex-col h-36 text-7xl w-40 text-center placeholder:text-white"
           />
           <label htmlFor="users">Active users</label>
         </div>
         <div>
           <input
+            placeholder={dashboardState.batteriesSwapStations}
             onChange={(e) => dispatch(addBatteries(Number(e.target.value)))}
             id="batteriesSwapStation"
             title="Swap station batteries"
             type="number"
-            className="bg-orange-400 border-l-2 flex flex-col h-36 text-7xl w-40 text-center"
+            className="bg-orange-400 border-l-2 flex flex-col h-36 text-7xl w-40 text-center placeholder:text-white"
           />
           <label htmlFor="batteriesSwapStation">Swap station batteries</label>
         </div>
--- src/features/dashboard/dashboardSlice.ts
+++ src/features/dashboard/dashboardSlice.ts
@@ -1,19 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
+
+const storedUsers = localStorage.getItem('addUsers');
+const users = storedUsers ? JSON.parse(storedUsers) : 0
+
+const sotoredBatteries = localStorage.getItem('addBatteries');
+const batteries = sotoredBatteries ? JSON.parse(sotoredBatteries) : 0;
 
 export const dashboardSlice = createSlice({
     name: 'dashboard',
     initialState: {
-        'activeUsers': 0,
-        'batteriesSwapStations': 0,
+        'activeUsers': users,
+        'batteriesSwapStations': batteries,
     },
     reducers: {
         addUsers: (state, action) => {
             state.activeUsers = action.payload;
+            localStorage.setItem('addUsers', JSON.stringify(state.activeUsers))
         },
         addBatteries: (state, action) => {
             state.batteriesSwapStations = action.payload;
+            localStorage.setItem('addBatteries', JSON.stringify(state.batteriesSwapStations));
         }
     }
 });
